f868092c9ee5db624369cfe231424d3141fbd7e0
surajkambledeveloper <suraj.kamble.developer@gmail.com>
2025-07-14 09:36:06 +0530

    initial commit

--- server/models/dynamicResumeModels/getModelByTemplate.js
+++ server/models/dynamicResumeModels/getModelByTemplate.js
@@ -1,18 +1,12 @@
 
 // add your tamplate here 
-const ResumeTemplate8 = require("../../models/resumeTemplatesModels/resumeTemplate8Model");
-const ResumeTemplate6=require("../../models/resumeTemplatesModels/resumeTemplate6Model")
-const ResumeTemplate9=require("../../models/resumeTemplatesModels/testingModel")
 const ResumeTemplate14=require("../../models/resumeTemplatesModels/resumeTemplate14Model")
 // Baad mein aur templates add karna ho to yaha karein
 
 const models = {
-  temp8: ResumeTemplate8,
-  temp6: ResumeTemplate6,
-  temp9: ResumeTemplate9,
+  
   temp14: ResumeTemplate14,
-  // temp1: ResumeTemplate1,
-  // temp2: ResumeTemplate2,
+  
 };
 
 const getModelByTemplate = (templateId) => {
